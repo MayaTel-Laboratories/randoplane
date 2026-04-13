e35a6487b73265699dd06fef20ad30ea5bcd23bf
maya ventura <kingforpa@gmail.com>
2026-04-13 11:01:43 -0400

    all the image selection code has been relocated, meaning that images/index.ts was now vestigial minus one line, which was easy to relocate to src/index.ts. also updated package.json

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,15 @@
 import { postImage } from './clients/at';
-import { getNextImage } from './images'; 
+import * as path from 'path';
 import * as dotenv from 'dotenv';
 dotenv.config();
+
 const CUTOFF_DATE = new Date('1950-10-01T00:00:00');
+
 function getDateFromFilename(filename: string): Date {
     const filenameNoJPG = filename.replace(/\.(JPG|jpeg|png|gif|bmp)$/i, "");
     return new Date(filenameNoJPG + 'T12:00:00'); 
 }
+
 function formatFullDate(dateObj: Date): string {
     const options: Intl.DateTimeFormatOptions = {
         weekday: 'long',
@@ -16,6 +19,7 @@ function formatFullDate(dateObj: Date): string {
     };
     return new Intl.DateTimeFormat('en-US', options).format(dateObj);
 }
+
 function generateAltText(dateObj: Date): string {
     const formattedDate = formatFullDate(dateObj);
     if (dateObj < CUTOFF_DATE) {
@@ -24,6 +28,7 @@ function generateAltText(dateObj: Date): string {
         return 'A Peanuts comic strip, drawn by Charles M. Schulz, originally released ' + formattedDate;
     }
 }
+
 function generateCaption(dateObj: Date): string {
   const formattedDate = formatFullDate(dateObj);
   if (dateObj < CUTOFF_DATE) {
@@ -36,17 +41,27 @@ function generateCaption(dateObj: Date): string {
     }
   }
 }
+
 async function main() {
-  const nextImage = await getNextImage(); 
-  const imageDate = getDateFromFilename(nextImage.imageName); 
+  const preSelectedPath = process.env.IMAGE_TO_POST;
+  if (!preSelectedPath) {
+    throw new Error('IMAGE_TO_POST environment variable is not set.');
+  }
+  console.log(`hello, index.ts here. i'm confirming that the RNG has selected ${preSelectedPath}`);
+
+  const imageName = path.basename(preSelectedPath);
+  const absolutePath = path.resolve(process.cwd(), preSelectedPath);
+  const imageDate = getDateFromFilename(imageName); 
   const postText = generateCaption(imageDate);
   const postAltText = generateAltText(imageDate);
+
   await postImage({
-    path: nextImage.absolutePath,
+    path: absolutePath,
     text: postText,
     altText: postAltText,
   });
 }
+
 main().catch(err => {
   console.error(err);
   process.exit(1);
